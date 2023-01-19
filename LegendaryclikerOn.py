7d1539450657697f2264da8552017896aec494a2
PetryckSlater <104164472+PetryckSlater@users.noreply.github.com>
2023-01-19 17:50:05 -0300

    Add files via upload

--- LegendaryclikerOn.py
+++ LegendaryclikerOn.py
@@ -1,5 +1,6 @@
 import time
 import pyautogui
+import os
 # primeiro teste usando metodos em um mini projeto!
 
 
@@ -18,12 +19,11 @@ class MouseLocal:
 
     def clicker(self):  # Função criada para o uso do clicker
         # primeira cordenada valor de x
-        self.local1 = int(input('Digite a primeira cordenada: '))
-        # segunda cordenada valor de y
-        self.local2 = int(input('Digite a Segunda: '))
+        input('coloque o mouse no local onde ocorrera os clicks e aperte enter! Não clique dentro do app que sera usado!:')
+        self.localx, self.localy = pyautogui.position()
         # Quantidade dos Clicks
         self.valorc1 = int(input('Digite a quantidade de Clicks: '))
-        pyautogui.click(x=self.local1, y=self.local2,
+        pyautogui.click(x=self.localx, y=self.localy,
                         clicks=self.valorc1, interval=0, button='left')
         print('Você foi devolvido a pagina inicial para caso tenha feito merda! ')
         return self.atual()
@@ -43,9 +43,46 @@ class MouseLocal:
             self.cont += 1
         if self.cont >= self.Valor:  # Criado para não fechar o programa assim que terminar o spawn
             return self.atual()
-
+    #Desenvolver copiar e colar
+    def copiar_colar(self):
+        quant = int(input('Quantidade por pasta: '))
+        input('Posicione o mouse dentro da pasta e dê enter: ')
+        x1, y1 = pyautogui.position()
+        print(x1,y1)
+        input('Posicione o mouse na dentro da segunda pasta: Enter ')
+        x2, y2 = pyautogui.position()
+        print(x2,y2)
+        cord1x = (x1)
+        cord1y = (y1) #armazena as cordenadas em uma tupla
+        cord2x = (x2)
+        cord2y = (y2)
+        input('Olá, devido a este programa esta na primeira fase, recomendo testar! Aperte enter para continuar! ')
+        i = 0
+        for i in range(quant):
+            pyautogui.click(x=cord1x, y=cord1y, clicks=1, interval=0, button='left')
+            pyautogui.hotkey('ctrl', 'x')
+            pyautogui.hotkey('Down')
+            pyautogui.click(x=cord2x, y=cord2y, clicks=1, interval=0, button='left')
+            pyautogui.hotkey('ctrl', 'v')
+            i+=1
+            if i == quant:
+                resp = input('Digite s caso deseje criar uma nova pasta ou n caso deseje apenas continuar: ')
+                if resp == 's':
+                    directory = str(input('Digite o diretorio do arquivo: '))
+                    namepast = str(input('Digite o nome da pasta: '))
+                    os.mkdir(f'{directory}/`{namepast}')
+                else:
+                    print('Você deseja continuar? a operação? \n 1) Para voltar para o organizador \n 2) para voltar ao menu \n 3)Sair ')
+                    rep1 = int(input('>>>'))
+                    if rep1 == 1:
+                        return self.copiar_colar()
+                    elif rep1 == 2:
+                        return self.atual()
+                    else:
+                        exit
+#Em desenvolvimento
     def atual(self):  # Usado para o controle das funções
-        print("digite um valor para escolher uma função:\n 1) ver localização do mouse! \n 2) Usar o um cliquer! \n 3) Usar o Spawn de Palavras!")
+        print("digite um valor para escolher uma função:\n 1) ver localização do mouse! \n 2) Usar o um cliquer! \n 3) Usar o Spawn de Palavras! \n 4) Digite 4 para acessar o organizador de pasta!")
         self.valor1 = int(input('>>>: '))
         key1 = MouseLocal()
         if self.valor1 == 1:
@@ -54,8 +91,10 @@ class MouseLocal:
             key1.clicker()
         elif self.valor1 == 3:
             key1.spawndepalavras()
+        elif self.valor1 == 4:
+            key1.copiar_colar()
         else:
-            return 0
+            exit
 
 
 mousekey = MouseLocal()
